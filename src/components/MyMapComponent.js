ceb74c3cdb6fc4c942e61de1e44ebec2bb76c211
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-21 12:19:09 -0500

    put map in a class

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -7,45 +7,70 @@ import  { compose, withProps, lifecycle } from 'recompose'
 import {withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer} from 'react-google-maps'
 
 
-const MyFancyComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAa_1I2oAv-cNMvVnW0EeAW6WaUeBniIhE&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap,
-  lifecycle({
 
-    componentDidMount() {
-      const DirectionsService = new google.maps.DirectionsService();
 
-      DirectionsService.route({
-        origin: new google.maps.LatLng(41.8507300, -87.6512600),
-        waypoints: [{location: 'williamsburg, VA', stopover: true}, {location: 'New York, NY', stopover: true},{location: 'Atlanta, GA', stopover: true}],
-        destination: new google.maps.LatLng(26.158147, -80.325408),
-        travelMode: google.maps.TravelMode.DRIVING,
-      }, (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-          this.setState({
-            directions: {...result},
-            markers: true
-          })
-        } else {
-          console.error(`error fetching directions ${result}`);
+
+
+class MyFancyComponent extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {}
+  }
+
+
+  render() {
+    console.log(this.props)
+
+    const MyMapComponent = compose(
+      withProps({
+        googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAa_1I2oAv-cNMvVnW0EeAW6WaUeBniIhE&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{ height: `400px` }} />,
+        mapElement: <div style={{ height: `100%` }} />,
+      }),
+      withScriptjs,
+      withGoogleMap,
+      lifecycle({
+
+        componentDidMount() {
+          const DirectionsService = new google.maps.DirectionsService();
+
+          DirectionsService.route({
+            origin: new google.maps.LatLng(41.8507300, -87.6512600),
+            waypoints: [{location: 'williamsburg, VA', stopover: true}, {location: 'New York, NY', stopover: true},{location: 'Atlanta, GA', stopover: true}],
+            destination: new google.maps.LatLng(26.158147, -80.325408),
+            travelMode: google.maps.TravelMode.DRIVING,
+          }, (result, status) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              this.setState({
+                directions: {...result},
+                markers: true
+              })
+            } else {
+              console.error(`error fetching directions ${result}`);
+            }
+          });
         }
-      });
-    }
-  })
-)(props =>
-  <GoogleMap
-    defaultZoom={3}
-    defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}
-  >
-    {props.directions && <DirectionsRenderer directions={props.directions} suppressMarkers={props.markers}/>}
-  </GoogleMap>
-);
+      })
+    )(props =>
+      <GoogleMap
+        defaultZoom={3}
+        defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}
+      >
+        {props.directions && <DirectionsRenderer directions={props.directions} suppressMarkers={props.markers}/>}
+      </GoogleMap>
+    );
+
+
+
+    return (
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
+    )
+  }
+}
 
 
 export default () => <MyFancyComponent />
