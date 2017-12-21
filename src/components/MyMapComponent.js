03b42e9e776f952009d6bb2380fc6ea7e5fc2a19
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-20 22:26:21 -0500

    got base to work

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -1,41 +1,21 @@
-import React from "react"
+import React from 'react'
 
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-
-class MyFancyComponent extends React.Component{
-
-render(){
-
-  const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+const MyFancyComponent = withScriptjs(withGoogleMap((props) =>
+  console.log(props)
   <GoogleMap
-  defaultZoom={8}
-  defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
   >
-  {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
   </GoogleMap>
 ))
 
-  return(
-    <MyMapComponent
-    isMarkerShown
-    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkkZymmIhFolJNzBjNf4EIxZTy8ORmujo&v=3.exp&libraries=geometry,drawing,places"
-    loadingElement={<div style={{ height: `100%` }} />}
-    containerElement={<div style={{ height: `400px` }} />}
-    mapElement={<div style={{ height: `100%` }} />}
-    />
-  )
-}
-
-
-
-
-
-
-}
-
-
-
-
-
-export default MyFancyComponent
+export default () => <MyFancyComponent
+  isMarkerShown
+  googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkkZymmIhFolJNzBjNf4EIxZTy8ORmujo&v=3.exp&libraries=geometry,drawing,places"
+  loadingElement={<div style={{ height: `100%` }} />}
+  containerElement={<div style={{ height: `400px` }} />}
+  mapElement={<div style={{ height: `100%` }} />}
+/>
