ed4257902619747e6c04101aa870f722a10de8f7
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2017-12-20 21:53:29 -0500

    got HOC to work for plain map

--- src/components/MyMapComponent.js
+++ src/components/MyMapComponent.js
@@ -1,74 +1,41 @@
 import React from "react"
 
-import { compose, withProps, lifecycle } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, DirectionsRenderer } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 
-const google = window.google;
+class MyFancyComponent extends React.Component{
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBkkZymmIhFolJNzBjNf4EIxZTy8ORmujo&v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap,
-  lifecycle({
-    componentDidMount() {
-      const DirectionsService = new google.maps.DirectionsService();
+render(){
 
-      DirectionsService.route({
-        origin: new google.maps.LatLng(41.8507300, -87.6512600),
-        destination: new google.maps.LatLng(41.8525800, -87.6514100),
-        travelMode: google.maps.TravelMode.DRIVING,
-      }, (result, status) => {
-        if (status === google.maps.DirectionsStatus.OK) {
-          this.setState({
-            directions: result,
-          });
-        } else {
-          console.error(`error fetching directions ${result}`);
-        }
-      });
-    }
-  })
-)(props =>
+  const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
-    defaultZoom={7}
-    defaultCenter={new google.maps.LatLng(41.8507300, -87.6512600)}
+  defaultZoom={8}
+  defaultCenter={{ lat: -34.397, lng: 150.644 }}
   >
-    {props.directions && <DirectionsRenderer directions={props.directions} />}
+  {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
   </GoogleMap>
-);
+))
 
-class MyFancyComponent extends React.PureComponent {
-  // state = {
-  //   isMarkerShown: false,
-  // }
-  //
-  // componentDidMount() {
-  //   this.delayedShowMarker()
-  // }
-  //
-  // delayedShowMarker = () => {
-  //   setTimeout(() => {
-  //     this.setState({ isMarkerShown: true })
-  //   }, 3000)
-  // }
-  //
-  // handleMarkerClick = () => {
-  //   this.setState({ isMarkerShown: false })
-  //   this.delayedShowMarker()
-  // }
-
-  render() {
-    debugger
-    return (
-      <MyMapComponent/>
-    )
-  }
+  return(
+    <MyMapComponent
+    isMarkerShown
+    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkkZymmIhFolJNzBjNf4EIxZTy8ORmujo&v=3.exp&libraries=geometry,drawing,places"
+    loadingElement={<div style={{ height: `100%` }} />}
+    containerElement={<div style={{ height: `400px` }} />}
+    mapElement={<div style={{ height: `100%` }} />}
+    />
+  )
 }
+
+
+
+
+
+
+}
+
+
+
+
 
 export default MyFancyComponent
