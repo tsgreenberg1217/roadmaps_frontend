cc7e00a13241517ce6820f1d6049c4fa9e38f041
tsgreenberg1217 <tsgreenberg1217@gmail.com>
2018-01-29 00:12:47 -0500

    ket

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,5 +2,5 @@
 // const baseUrl = 'https://safe-caverns-60257.herokuapp.com/api/v1'
 
 module.exports = {
-  url: 'http://localhost:3000/api/v1'
+  url: 'https://myroadmaps.herokuapp.com/api/v1'
 }
